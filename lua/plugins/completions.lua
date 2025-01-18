f3d3b59caccbd3b8ab181e32f431af4dd1e10277
sushi057 <suvash2077@gmail.com>
2025-01-19 00:05:27 +0545

    add keymaps for docs scroll

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -28,6 +28,8 @@ return {
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
