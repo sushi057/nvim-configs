c7f621c8e8a4b4e63b69ac0dee03c0b3f04e49f9
sushi057 <suvash2077@gmail.com>
2025-01-30 23:54:14 +0545

    chore: cleanup

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,9 +5,11 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
+    -- { "3rd/image.nvim", opts = {} },
   },
   config = function()
+    -- require("nvim-tree").setup({})
     vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal right<CR>", {})
     vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-  end
+  end,
 }
--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,46 +1,47 @@
 return {
-  "xiyaowong/transparent.nvim",
-  config = function()
-    require("transparent").setup({
-      -- table: default groups
-      groups = {
-        "Normal",
-        "NormalNC",
-        "Comment",
-        "Constant",
-        "Special",
-        "Identifier",
-        "Statement",
-        "PreProc",
-        "Type",
-        "Underlined",
-        "Todo",
-        "String",
-        "Function",
-        "Conditional",
-        "Repeat",
-        "Operator",
-        "Structure",
-        "LineNr",
-        "NonText",
-        "SignColumn",
-        "CursorLine",
-        "CursorLineNr",
-        "StatusLine",
-        "StatusLineNC",
-        "EndOfBuffer",
-        "Harpoon"
-      },
-      -- table: additional groups that should be cleared
-      extra_groups = {
-        "NeoTreeNormal",
-        "NeoTreeNormalNC",
-      },
-      -- table: groups you don't want to clear
-      exclude_groups = {},
-      -- function: code to be executed after highlight groups are cleared
-      -- Also the user event "TransparentClear" will be triggered
-      on_clear = function() end,
-    })
-  end,
+	"xiyaowong/transparent.nvim",
+	config = function()
+		require("transparent").setup({
+			-- table: default groups
+			groups = {
+				"Normal",
+				"NormalNC",
+				"Comment",
+				"Constant",
+				"Special",
+				"Identifier",
+				"Statement",
+				"PreProc",
+				"Type",
+				"Underlined",
+				"Todo",
+				"String",
+				"Function",
+				"Conditional",
+				"Repeat",
+				"Operator",
+				"Structure",
+				"LineNr",
+				"NonText",
+				"SignColumn",
+				"CursorLine",
+				"CursorLineNr",
+				"StatusLine",
+				"StatusLineNC",
+				"EndOfBuffer",
+				"Harpoon",
+			},
+			-- table: additional groups that should be cleared
+			extra_groups = {
+				"NeoTreeNormal",
+				"NeoTreeNormalNC",
+				"NeoTreeVertSplit",
+			},
+			-- table: groups you don't want to clear
+			exclude_groups = {},
+			-- function: code to be executed after highlight groups are cleared
+			-- Also the user event "TransparentClear" will be triggered
+			on_clear = function() end,
+		})
+	end,
 }
